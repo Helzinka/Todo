ddf22c93697fe21b6834fdc5bc0598f414f99d02
Helzinka <leveque.fra@gmail.com>
2022-10-31 16:52:05 +0100

    add new row function

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 ## Todo app
 
-Goal is to make a fonctional todo app with query selector and HTML/CSS
+Goal is to make a fonctional todo app with query selector from javascript and HTML/CSS
 
-# Wip
+### Wip
+
+I started from a design found [here](https://dribbble.com/shots/18115059-LaunchPad-Todo)
 
 ![Screenshot](./img/wip.png)
--- script.js
+++ script.js
@@ -5,28 +5,80 @@ const $$ = document.querySelectorAll.bind(document);
 
 // Selectors
 
-let add = $(".add");
-let add_modal = $(".add_modal");
-let add_btn = $(".add_btn");
-let cancel_btn = $(".cancel_btn");
+let Add = $(".add");
+let Add_modal = $(".add_modal");
+let Add_btn = $(".add_btn");
+let Cancel_btn = $(".cancel_btn");
+let Content = $("#content");
+let Contact_form = $("#contact_form");
 
-// Display or not a task
+// Display a task modal from add button
 
 let is_add_open = false;
 
-add.addEventListener("click", (data) => {
+Add.addEventListener("click", (data) => {
 	if (!is_add_open) {
-		add_modal.classList.remove("none");
+		Add_modal.classList.remove("none");
 		is_add_open = true;
 	}
 });
 
-add_btn.addEventListener("click", (data) => {
-	add_modal.className += " none";
+// Hide task modal if candel is performed
+Cancel_btn.addEventListener("click", (data) => {
+	Add_modal.className += " none";
 	is_add_open = false;
 });
 
-cancel_btn.addEventListener("click", (data) => {
-	add_modal.className += " none";
+// Get data from formdata on submit event
+Contact_form.addEventListener("submit", (data) => {
+	// Prevent reload
+	data.preventDefault();
+
+	// Hide modal
+	Add_modal.className += " none";
 	is_add_open = false;
+
+	// Get data in FormData and return a object
+	const myFormData = new FormData(data.target);
+	let data_form = Object.fromEntries(myFormData.entries());
+
+	return createRow(data_form);
 });
+
+function createRow(data) {
+	console.log(data.task.length);
+	if (data.task.length > 2) {
+		let template_todo = `
+			<div class="row">
+				<div class="completed_container">
+					<input type="checkbox" class="completed" />
+					<label for="checkbox"></label>
+				</div>
+				<div id="card">
+					<div class="title">
+						${data.task}
+					</div>
+					<div class="description">
+						${data.desc}
+					</div>
+					<div class="meta">
+						<div class="date">
+							<i class="fa-regular fa-calendar"></i>
+							<span>6 avril</span>
+						</div>
+						<div class="autor">
+							<i class="fa-regular fa-user"></i>
+							<span>Franz lvq</span>
+						</div>
+					</div>
+				</div>
+			</div>`;
+
+		Content.innerHTML += template_todo;
+	} else {
+		Content.innerHTML = Content.innerHTML;
+	}
+	// else {
+	// 	infos("Title is empty", "red");
+	// }
+}
